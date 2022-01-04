02f5e30c8fa034bf99844e3b8252a744d17f8303
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2022-01-04 13:44:42 +0200

    Moved models to its own files

--- device.go
+++ device.go
@@ -9,34 +9,11 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-type statusNet struct {
-	Hostname   string  `json:"Hostname"`
-	IPAddress  string  `json:"IPAddress"`
-	Gateway    string  `json:"Gateway"`
-	Subnetmask string  `json:"Subnetmask"`
-	DNSServer1 string  `json:"DNSServer1"`
-	DNSServer2 string  `json:"DNSServer2"`
-	Mac        string  `json:"Mac"`
-	Webserver  int     `json:"Webserver"`
-	WifiPower  float32 `json:"WifiPower"`
-}
-
-type Device struct {
-	topic      string
-	fullTopic  string
-	mqttClient mqtt.Client
-	online     bool
-	UptimeSec  uint      `json:"UptimeSec"`
-	LoadAvg    uint      `json:"LoadAvg"`
-	Timezone   string    `json:"Timezone"`
-	StatusNet  statusNet `json:"StatusNET"`
-}
-
 // NewDevice will create a new Device
 func NewDevice(topic, fullTopic string, mqttClient mqtt.Client) *Device {
 	o := &Device{
-		topic:      topic,
-		fullTopic:  fullTopic,
+		Topic:      topic,
+		Fulltopic:  fullTopic,
 		mqttClient: mqttClient,
 	}
 	subscribeTopics := make(map[string]byte)
@@ -51,19 +28,22 @@ func (d *Device) MessageHandler(client mqtt.Client, msg mqtt.Message) {
 	fmt.Printf("%s: %s\n", msg.Topic(), msg.Payload())
 	if strings.HasSuffix(msg.Topic(), "LWT") {
 		if string(msg.Payload()) == "Online" {
-			d.online = true
+			d.Online = true
 			d.SendCmnd("STATUS", "5")
 			d.SendCmnd("TIMEZONE", "")
 		} else {
-			d.online = false
+			d.Online = false
 		}
 	} else if strings.HasSuffix(msg.Topic(), "STATUS5") || strings.HasSuffix(msg.Topic(), "STATE") || strings.HasSuffix(msg.Topic(), "RESULT") {
-		d.unmarshalPayload(msg.Payload())
+		err := d.unmarshalPayload(msg.Payload())
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
 func (d *Device) SendCmnd(cmnd string, payload string) {
-	mqttTopic := strings.Replace(strings.Replace(d.fullTopic, "%prefix%", "cmnd", 1), "%topic%", d.topic, 1) + cmnd
+	mqttTopic := strings.Replace(strings.Replace(d.Fulltopic, "%prefix%", "cmnd", 1), "%topic%", d.Topic, 1) + cmnd
 	d.mqttClient.Publish(mqttTopic, 1, false, payload)
 }
 
