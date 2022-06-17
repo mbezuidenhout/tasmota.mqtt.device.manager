cab69f9c898e43c9a514721e302969c91498cfc5
Marius Bezuidenhout <marius.bezuidenhout@gmail.com>
2022-06-17 14:31:14 +0200

    Maybe fixed segfault in sensor.go line 28

--- sensor.go
+++ sensor.go
@@ -23,33 +23,37 @@ func (d *Device) unmarshalSensorPayload(payload []byte) error {
 	}
 	r2 := regexp.MustCompile(`{\"ZbReceived\":{\"([x0-9A-F]+)\":(.*)}}`)
 	matches2 := r2.FindAllStringSubmatch(string(payload), -1)
-	if len(matches2) == 1 && len(matches2[0]) == 3 {
+	if len(matches2) == 1 && len(matches2[0]) == 3 && d.Sensors["Zigbee"][matches2[0][1]] != nil {
 		if len(d.Sensors["Zigbee"]) == 0 {
 			d.Sensors["Zigbee"] = make(map[string]interface{})
 		}
 		x := ZigbeeTH01{}
-		y := d.Sensors["Zigbee"][matches2[0][1]].(ZigbeeTH01)
-		err := json.Unmarshal([]byte(matches2[0][2]), &x)
-		y.LastSeen = 0
-		y.LastSeenEpoch = time.Now().UTC().Unix()
-		if x.Name != y.Name {
-			y.Name = x.Name
+		if sensor, ok := d.Sensors["Zigbee"][matches2[0][1]]; ok { // Sensor exists in array
+			y := sensor.(ZigbeeTH01)
+			err := json.Unmarshal([]byte(matches2[0][2]), &x)
+			y.LastSeen = 0
+			y.LastSeenEpoch = time.Now().UTC().Unix()
+			if x.Name != y.Name {
+				y.Name = x.Name
+			}
+			if x.Temperature != 0 {
+				y.Temperature = x.Temperature
+			}
+			if x.Humidity != 0 {
+				y.Humidity = x.Humidity
+			}
+			if x.BatteryPercentage != 0 {
+				y.BatteryPercentage = x.BatteryPercentage
+			}
+			if x.LinkQuality != y.LinkQuality {
+				y.LinkQuality = x.LinkQuality
+			}
+			d.Sensors["Zigbee"][matches2[0][1]] = y
+			fmt.Println(d.Sensors["Zigbee"][matches2[0][1]].(ZigbeeTH01))
+			return err
+		} else { // First time this sensor is seen
+			d.SendCmnd("ZbInfo", "")
 		}
-		if x.Temperature != 0 {
-			y.Temperature = x.Temperature
-		}
-		if x.Humidity != 0 {
-			y.Humidity = x.Humidity
-		}
-		if x.BatteryPercentage != 0 {
-			y.BatteryPercentage = x.BatteryPercentage
-		}
-		if x.LinkQuality != y.LinkQuality {
-			y.LinkQuality = x.LinkQuality
-		}
-		d.Sensors["Zigbee"][matches2[0][1]] = y
-		fmt.Println(d.Sensors["Zigbee"][matches2[0][1]].(ZigbeeTH01))
-		return err
 	}
 	return nil
 }
